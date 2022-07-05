ece004362abde99ef8d78ca8f823ae41200cbea1
Jupilian <Ropersjulien@gmail.com>
2022-07-05 17:38:39 +0200

    Added "!l" alias to "!last" to avoid triggering other bots

--- parking.py
+++ parking.py
@@ -13,7 +13,7 @@ async def on_ready():
     print("Logged in as {0.user}".format(client))
 
 
-@client.command()
+@client.command(aliases=['l'])
 async def last(ctx):
     user = userhandler.get_user(ctx.author.id)
     if user != "error":   
